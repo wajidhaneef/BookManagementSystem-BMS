bb506670e3f03cc7acecd573236584213b590403
Wajid Ali <wajid.haneef@devsinc.com>
2023-06-10 16:14:04 +0500

    Bug fixing

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -37,7 +37,7 @@ namespace BookManagementSystem_BMS.Controllers
 
             var categories = _dbContext.Categories.ToList();
             var books = _dbContext.Books.ToList();
-
+            var chapters = _dbContext.Chapters.ToList();
             var coverpages = _dbContext.CoverPages.ToList();
             List<CoverPageViewModel> CoverImages = new List<CoverPageViewModel>();
             foreach (var coverPage in coverpages)
@@ -60,7 +60,8 @@ namespace BookManagementSystem_BMS.Controllers
                         //CoverImage = MyBitmap, BookId=coverPage.BookId,
                         BookName= books.FirstOrDefault(b=>b.BookID==coverPage.BookId).BookName,
                         CategoryId = books.FirstOrDefault(b => b.BookID == coverPage.BookId).CategoryID,
-                        ImageContent = imgBytes
+                        ImageContent = imgBytes,
+                        BookId = coverPage.BookId
                     });
                 }
             }
@@ -72,6 +73,14 @@ namespace BookManagementSystem_BMS.Controllers
             {
                 AllCategories = categories,
                 Books = books,
+                Chapters = chapters,
+                SelectedBook = books.First().BookName,
+                SelectedCategory = categories.First().CategoryName,
+                SelectedCategoryId = categories.First().CategoryID,
+                SelectedBookId = books.First().BookID,
+                SelectedChapterId = chapters.First().ChapterID,
+                SelectedChapter = chapters.First().ChapterName,
+                SelectedChapterContent = chapters.First().Content,
                 CoverPages = CoverImages,
                 
 
@@ -82,25 +91,43 @@ namespace BookManagementSystem_BMS.Controllers
         
 
         // GET: BookController/Details/5
-        public ActionResult Details(int categoryId)
+        public ActionResult Details(BookViewModel viewModel, int categoryId)
         {
+            var books = _dbContext.Books.Where(b => b.CategoryID == categoryId).ToList();
+            int bookId = books.First().BookID;
+            var chapters = _dbContext.Chapters.Where(c => c.BookID == bookId).ToList();
+            
             var categories = _dbContext.Categories.ToList();
-            var viewModel = new BookViewModel
-            {
-                AllCategories = categories,
-                SelectedCategory = categories.FirstOrDefault(b => b.CategoryID == categoryId, categories.First()).CategoryName,
-                SelectedCategoryId = categoryId
-            };
+            viewModel.AllCategories = categories;
+            viewModel.Books = books;
+            viewModel.SelectedCategoryId = categoryId;
+            viewModel.Chapters = chapters;
+            viewModel.SelectedChapterContent = chapters.First().Content;
+            //var viewModel = new BookViewModel
+            //{
+            //    AllCategories = categories,
+            //    SelectedCategory = categories.FirstOrDefault(b => b.CategoryID == categoryId, categories.First()).CategoryName,
+            //    SelectedCategoryId = categoryId
+            //};
 
             return View("BookOverview",viewModel);
         }
+        //[HttpGet]
+        //public ActionResult BookOverviewByCover(IFormCollection collection)
+        //{
+        //    string selectedChapter = collection["username"];
+        //    var viewModel = new BookViewModel();
+        //    Console.WriteLine(selectedChapter);
+        //    return View("BookOverview");
+        //}
+        [HttpGet]
         public ActionResult BookOverviewByCover(int bookId, int categoryId, int chapterId, int home)
         {
             var categories = _dbContext.Categories.ToList();
             var chapters = _dbContext.Chapters.ToList();
             var SelectedBookId = 0;
             var SelectedChapterId = 0;
-            if (home==1)
+            if (home == 1)
             {
                 categoryId = _dbContext.Books.FirstOrDefault(b => b.BookID == bookId).CategoryID;
             }
@@ -109,8 +136,8 @@ namespace BookManagementSystem_BMS.Controllers
                 SelectedBookId = _dbContext.Books.FirstOrDefault(b => b.CategoryID == categoryId).BookID;
                 SelectedChapterId = chapters.FirstOrDefault(b => b.BookID == bookId, chapters.First()).ChapterID;
             }
-            
-            
+
+
             var viewModel = new BookViewModel
             {
                 AllCategories = categories,
@@ -142,40 +169,50 @@ namespace BookManagementSystem_BMS.Controllers
 
         }
 
+        [HttpGet]
+        public ActionResult BookOverview(BookViewModel viewModel)
+        {
+
+            viewModel.SelectedCategoryId = viewModel.Books.FirstOrDefault(b => b.BookID == viewModel.SelectedBookId).CategoryID;
+            
+            return View("BookOverview", viewModel);
+            
+
+        }
         // GET: Book/GetBooksByCategory
-        public ActionResult GetBooksByCategory(int categoryId, int bookId)
+        public ActionResult GetBooksByCategory(BookViewModel viewModel)
         {
             //var books = _dbContext.Books.Where(b => b.CategoryID == categoryId).ToList();
-            var books = _dbContext.Books.Where(b => b.CategoryID == categoryId).ToList();
-            var viewModel = new BookViewModel
-            {
-                Books = books,
-                SelectedBook = books.FirstOrDefault(b => b.BookID == bookId, books.First()).BookName,
-                SelectedBookId = bookId!=0?bookId :books.First().BookID,
-            };
+            //var books = _dbContext.Books.Where(b => b.CategoryID == categoryId).ToList();
+            //var viewModel = new BookViewModel
+            //{
+            //    Books = books,
+            //    SelectedBook = books.FirstOrDefault(b => b.BookID == bookId, books.First()).BookName,
+            //    SelectedBookId = bookId!=0?bookId :books.First().BookID,
+            //};
 
             return PartialView("_BooksDropdown", viewModel);
         }
         
         // GET: Book/GetChaptersByBook
-        public ActionResult GetChaptersByBook(int bookId, int chapterId)
+        public ActionResult GetChaptersByBook(BookViewModel viewModel)
         {
-            //var chapters = _dbContext.Chapters.Where(c => c.BookID == bookId).ToList();
-            var chapters = _dbContext.Chapters.Where(b => b.BookID == bookId).ToList();
-            var viewModel = new BookViewModel
-            {
-                Chapters = chapters,
-                SelectedChapter = chapters.FirstOrDefault(b => b.ChapterID == chapterId, chapters.First()).ChapterName,
-                SelectedChapterId = chapterId!=0?chapterId: chapters.First().ChapterID,
-            };
+            //chapters = _dbContext.Chapters.Where(c => c.BookID == bookId).ToList();
+            //var viewModel = new BookViewModel
+            //{
+            //    Chapters = chapters,
+            //    SelectedChapter = chapters.FirstOrDefault(b => b.ChapterID == chapterId, chapters.First()).ChapterName,
+            //    SelectedChapterId = chapterId!=0?chapterId: chapters.First().ChapterID,
+            //};
 
             return PartialView("_ChaptersDropdown", viewModel);
         }
 
         // GET: Book/GetChapterContent
-        public ActionResult GetChapterContent(int chapterId)
+        public ActionResult GetChapterContent(BookViewModel viewModel)
         {
-            var chapter = _dbContext.Chapters.FirstOrDefault(c => c.ChapterID == chapterId);
+            //var chapter = _dbContext.Chapters.FirstOrDefault(c => c.ChapterID == chapterId);
+            var chapter = viewModel.Chapters.FirstOrDefault(c => c.ChapterID == viewModel.SelectedChapterId);
 
             if (chapter != null)
             {
